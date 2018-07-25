04727b9b61ab18d7be34658dd612d8682e18b3d3
ImenDridi <zaghdoudi_imen@hotmail.fr>
2018-07-25 15:48:55 +0300

    Services added and sending test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { ChatAppComponent } from './components/chat-app/chat-app.component';
 import { AboutComponent } from './components/about/about.component';
+import { ParentChatBoxComponent } from './components/parent-chat-box/parent-chat-box.component';
 import { ChildChatBoxComponent } from './components/child-chat-box/child-chat-box.component';
+import { ChattingService } from './services/chatting.service';
+
+
 
 
 @NgModule({
@@ -16,7 +20,10 @@ import { ChildChatBoxComponent } from './components/child-chat-box/child-chat-bo
     WrapperComponent,
     ChatAppComponent,
     AboutComponent,
+    ParentChatBoxComponent,
     ChildChatBoxComponent,
+
+
 
   ],
   imports: [
@@ -24,7 +31,8 @@ import { ChildChatBoxComponent } from './components/child-chat-box/child-chat-bo
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+
+  providers: [ChattingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
